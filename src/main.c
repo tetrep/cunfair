13b9eb19b4455c8e2dd1fadbfd56c190021af767
Paul Tetreau <paul@matasano.com>
2015-04-23 17:46:13 -0700

    basic setup

--- src/main.c
+++ src/main.c
@@ -1,3 +1,23 @@
-int main () {
+#include <stdlib.h>
+
+void print_usage(char *name) {
+  fprint("usage: %s [num_samples] [keystream_length]\n");
+}
+
+int main (int argc, char *argv[]) {
+  if (argc != 3) {
+    fprintf(stderr, "error: invalid number of arguments\n");
+    print_usage(argv[0]);
+    abort();
+  }
+
+  int num_samples = -1;
+  int keystream_length = -1;
+
+  num_samples = atoi(argv[1]);
+  keystream_length = atoi(argv[2]);
+
+  fprintf(stdout, "num_samples : %i\nkeystream_length : %i\n", num_samples, keystream_length);
+
   return 0;
 }
