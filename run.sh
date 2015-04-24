c6d65cf116e267446a91f19dbc6c04f3707a7d2f
Paul Tetreau <paul@matasano.com>
2015-04-23 18:13:55 -0700

    basic structure for rc4, just need to init the cipher and make some keystream

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./build.sh && ./bin/main.exe
+./build.sh && ./bin/main.exe ${1}${2}
--- src/main.c
+++ src/main.c
@@ -1,13 +1,36 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-void print_usage(char *name) {
-  fprint("usage: %s [num_samples] [keystream_length]\n");
+int cunfair_rc4_keystream(int keystream_length, char *buffer) {
+  return 0;
+}
+
+int cunfair_rc4_gen (int num_samples, int keystream_length) {
+  int err = -1;
+  char *buffer = NULL;
+
+  // make buffer large enough to store the requested keystream size
+  if (NULL == (buffer = malloc(keystream_length))) {
+    fprintf(stderr, "error: Could not allocate memory for keystream; please download more RAM.\n");
+    return -1;
+  }
+
+  if (-1 == (err = cunfair_rc4_keystream(keystream_length, buffer))) {
+    fprintf(stderr, "error: Could not generator rc4 keystream of %i bytes.\n", keystream_length);
+    return err;
+  }
+
+  return 0;
+}
+
+void cunfair_print_usage (char *name) {
+  fprintf(stderr, "usage: %s [num_samples] [keystream_length]\n", name);
 }
 
 int main (int argc, char *argv[]) {
   if (argc != 3) {
     fprintf(stderr, "error: invalid number of arguments\n");
-    print_usage(argv[0]);
+    cunfair_print_usage(argv[0]);
     abort();
   }
 
@@ -19,5 +42,5 @@ int main (int argc, char *argv[]) {
 
   fprintf(stdout, "num_samples : %i\nkeystream_length : %i\n", num_samples, keystream_length);
 
-  return 0;
+  return cunfair_rc4_gen(num_samples, keystream_length);
 }
